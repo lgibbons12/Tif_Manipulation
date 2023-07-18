2f171496c0b9ab749d536997472c430849143ce4
Liam Gibbons <112109780+lgibbons12@users.noreply.github.com>
2023-07-18 10:34:16 -0400

    Update scraping.py

--- scraping.py
+++ scraping.py
@@ -1,24 +1,34 @@
+#imports
 import requests
 from bs4 import BeautifulSoup
 import getting_data
-# Scrape the webpage and find the URLs of the buttons you want to click
-url = "https://www.earthenv.org/landcover"  # Replace with the actual URL
+import pandas as pd
+import numpy as np
+
+#get the webpage
+url = "https://www.earthenv.org/landcover"  
 response = requests.get(url)
 
+#get the html of the webpage
 soup = BeautifulSoup(response.content, "html.parser")
 
-# Find the buttons or elements containing the URLs
-button_elements = soup.find(id = "landcoverfull")  # Replace with the appropriate selector
+#select the table with the downloads we need
+table = soup.find(id = "landcoverfull")  
 
+#get every row 
+td_elements = table.find_all("td")
 
-td_elements = button_elements.find_all("td")
+#urls of all downloads
 button_urls = []
+
+#go through each row and save the download link to the button_urls list
 for td in td_elements:
     link = td.find("a")
     if link:
         first_link = link["href"]
         button_urls.append(first_link)
 
+#filenames list used for filenames
 filenames = [
     "Evergreen_Deciduous_Needleleaf_Trees.tif",
     "Evergreen_Broadleaf_Trees.tif",
@@ -33,12 +43,15 @@ filenames = [
     "Barren.tif",
     "Open_Water.tif"
 ]
+
+
+
 for idx, button_url in enumerate(button_urls):
     # Download the file
     file_response = requests.get(button_url)
-    # Extract the filename from the URL or any other method
     
-    # Save the file to disk
+    
+    # Save the file to data directory
     with open(f"data/{filenames[idx]}", "wb") as file:
         file.write(file_response.content)
 
@@ -46,25 +59,27 @@ for idx, button_url in enumerate(button_urls):
 
     print(f"File {filenames[idx]} downloaded and unzipped successfully.")
 
-import pandas as pd
-import numpy as np
+
+###
 
 
-
-
-
-
+#read in data
 df = pd.read_csv('data.csv')
 
-
+#create keys variable and column in dataframe
+#we will use this to map the data together
 df['keys'] = (keys := np.arange(len(df)))
 values = []
 
-
+#now we create a list of tuples with latitude and longitude that we can use
+#these tuples will be how we access the data from the tif file
 coordinates = [(lat, lon) for lat, lon in df[["Latitude", "Longitude"]].to_records(index=False)]
+
 
 for i in range(len(filenames)):
     name = f"data/{filenames[i]}"
+
+    #now we go into the getting data module to get the data from the tif file
     df = getting_data.grab(coordinates, df, name)
 
 df.to_csv("final.csv", index=False)
